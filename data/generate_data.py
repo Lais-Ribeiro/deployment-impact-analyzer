55e819717bc40f4cb25320129bfaee2b32a12af9
Lais-Ribeiro <74383107+Lais-Ribeiro@users.noreply.github.com>
2026-08-02 11:40:46 -0300

    Connect data generation script to Elasticsearch

--- data/generate_data.py
+++ data/generate_data.py
@@ -1,5 +1,10 @@
 import random
 from datetime import datetime, timedelta
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch(["http://localhost:9200"])
+es.indices.create(index="logs")
+
 
 def lista_alaeatoria(campo):
 
@@ -16,23 +21,20 @@ def gerar_timestamp_aleatorio():
 
 def generate_logs(num_logs):
 
-    lista_logs = []
     lista_status_codes = [200, 201, 204, 400, 412, 421, 500, 504, 550]
     lista_endpoints = ["/api/v1/users", "/api/v1/products", "/api/v1/orders"]
     lista_erros = ["Timeout", "Database error", "Connection refused", "Unauthorized"]
 
     for i in range(num_logs):
+
         status = lista_alaeatoria(lista_status_codes)
         tem_erro = status >= 500
-        
-        logs = {
+        es.index(index="logs", body={
             "timestamp": gerar_timestamp_aleatorio(),
             "endpoint": lista_alaeatoria(lista_endpoints),
             "status_code": status,
             "latencia_ms": random.randint(50, 2000),
-            "tipo_erro": lista_alaeatoria(lista_erros) if tem_erro else None
-        }
-        lista_logs.append(logs)
-    return lista_logs
+            "tipo_erro": lista_alaeatoria(lista_erros) if tem_erro else None})
+    return f"✅ {num_logs} registros enviados pro Elasticsearch!"
 
 print(generate_logs(10))
